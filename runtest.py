a579f43a257dbc59bf78eeae9ec43d835a5ec911
pikaurd <pikaurd@gmail.com>
2011-08-28 16:21:13 +0800

    major feature complete. Will combile them to applection

--- runtest.py
+++ runtest.py
@@ -7,10 +7,13 @@
 ###
 
 import os
+import re
 import unittest
 
-from EpubBuilder import EpubBuilder
-from EpubBuilder import Chapter
+from epubbuilder import *
+
+from pikaurdlib.xmldocument import XMLElement
+from pikaurdlib.util import encodeXML
 
 
 class TestBuildEpub(unittest.TestCase):
@@ -39,15 +42,14 @@ class TestChapter(unittest.TestCase):
     c = Chapter(1, 'chapter1')
     print(c)
     
-  @unittest.skip('skip')
   def test_convertContentToXML(self):
-    content = '''Hello epub & python world!
+    content = '''Hello epub python world!
 This is my first generated chapter'''
     c = Chapter(1, 'chapter1', content)
-    expect = 'Hello epub &amp; python world!<br/>This is my first generated chapter'
+    expect = 'Hello epub python world!<br/>This is my first generated chapter'
     self.assertEqual(expect, c.content.create())
 
-  @unittest.skip('skip')
+  @unittest.skip("run once enough")
   def test_write(self):
     self.maxDiff = 6000
     with open('devlog.txt') as f:
@@ -57,17 +59,74 @@ This is my first generated chapter'''
       expect = f.read()
     self.assertEqual(expect, ch.__str__())
 
-class TestParseTxt(unittest.TestCase):
-  def test_parse(self):
-    parseTxt = ParseTxt()
-    with open('epubDir/md.txt') as f:
-      txt = f.read()
-    charpters = parseTxt.parse(txt)
+@unittest.skip('skip')
+class TestTextSplitter(unittest.TestCase):
+  def test_getHead(self):
+    splitter = TextSplitter()
+    with open('epubDir/head.txt') as f:
+      head = splitter.getHead(f)
+    print(head)
 
 
+#@unittest.skip('skip')
+class TestTempSplitAndCreateFile(unittest.TestCase):
+  def test_hasChapter(self):
+    self.assertEqual('chapter', getChapter('fdajklfsdj#[chapter:chapter]#'))
 
+  def test_addImageIfNeed(self):
+    self.assertEqual('<img src="path/innerPath/img.jpg"/>', addImageIfNeed('ax#[img:innerPath/img.jpg]#suffix', 'path'))
+    self.assertEqual('def test_split(self):', addImageIfNeed('def test_split(self):', 'path'))
 
+  def test_isBlank(self):
+    self.assertTrue(isBlank(''))
+    self.assertTrue(isBlank('   '))
+    self.assertFalse(isBlank('1'))
+    self.assertFalse(isBlank(' ', True))
 
+  @unittest.skip('tmp')
+  def test_txtParseAndCreateChapter(self):
+    txtParseAndCreateChapter('epubDir/head.txt', imgBaseDir='epubDir')
+
+  @unittest.skip('tmp')
+  def test_split(self):
+    buffer = ''
+    index = 0
+    title = 'info'
+    with open('epubDir/head.txt') as f:
+      tmp = encodeXML(f.readline())
+      while not isBlank(tmp):
+        while isBlank(getChapter(tmp)) and not isBlank(tmp, True):
+          buffer += addImageIfNeed(tmp, 'epubDir')
+          tmp = f.readline()
+
+        print('chapter change:\t'+title)
+        buffer = buffer.strip()
+        fileName = 'chapter-{}'.format(index)
+        if len(buffer) > 0:
+          ch = Chapter(fileName, title, buffer)
+          ch.write()
+          buffer = ''
+          index += 1
+          title = getChapter(tmp)
+          tmp = f.readline()
+
+class TestTocncx(unittest.TestCase):
+  @unittest.skip('succ')
+  def test_create(self):    
+    toc = TOCncx('title', ['ch1', 'ch2', 'ch3'], 'thisiduid')
+    print(toc.create())
+    
+class TestContentOpf(unittest.TestCase):
+#  @unittest.skip(1)
+  def test_create(self):
+    opf = ContentOpf('title', 'creator', 'uid', language='zh-CN')
+    print(opf.create())
+
+  def test_createItem(self):
+    opf = ContentOpf('title', 'creator', 'uid', language='zh-CN')
+    ncx = opf.createItem('ncx', 'toc.ncx', 'application/x-dtbncx+xml').create()
+    self.assertEqual('<item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>', ncx)
+    
 
 if __name__ == "__main__":
   unittest.main()
